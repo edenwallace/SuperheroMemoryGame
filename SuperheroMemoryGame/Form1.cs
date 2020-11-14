0f430ce475d4cb91f7664fbf62ea51acf51b585a
Eden Wallace <wallac33@nsuok.edu>
2020-11-13 18:21:56 -0600

    Add project files.

---
+++ SuperheroMemoryGame/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperheroMemoryGame
+{
+    public partial class Form1 : Form
+    {
+        bool allowClick = false; //Boolean allowClick means you can not click picture box
+        PictureBox firstGuess;
+        Random rnd = new Random();
+        Timer clickTimer = new Timer(); //new instance of random number generator class
+        int time = 60; //time to play the game
+        Timer endTime;
+        Timer timer = new Timer { Interval = 1000 }; //time interval for timer
+        
+
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        private PictureBox[] pictureBoxes
+        {
+            get { return Controls.OfType<PictureBox>().ToArray(); } //adds picture boxes to an array
+        }
+
+        private static IEnumerable<Image> images //creates access to query images, links images from resources
+        {
+            get
+            {
+                return new Image[] //returns a new image from the array
+                {
+                    Properties.Resources.img1,
+                    Properties.Resources.img2,
+                    Properties.Resources.img3,
+                    Properties.Resources.img4,
+                    Properties.Resources.img5,
+                    Properties.Resources.img6,
+                    Properties.Resources.img7,
+                    Properties.Resources.img8,
+                };
+            }
+        }
+
+        private void startGameTimer() //starts timer and uses if statement for 
+        {
+            timer.Start();
+            timer.Tick += delegate
+            {
+                time--;
+                if (time < 0) //if player runs out of time, a message box displays out of time
+                {
+                    timer.Stop();
+                    MessageBox.Show("Out of time");
+                    ResetImages();
+                }
+
+                var ssTime = TimeSpan.FromSeconds(time); //time in seconds
+                label1.Text = "00: " + time.ToString();
+            };
+        }
+
+            private void ResetImages() //resets picture boxes, allows player to play again
+            {
+                foreach (var pic in pictureBoxes) 
+                {
+                    pic.Tag = null; 
+                    pic.Visible = true; //makes pictures visible again
+                }
+
+                HideImages(); //calls function we created
+                setRandomImages(); //calls function we created
+                time = 60; 
+                timer.Start(); //starts timer again
+            }
+
+            private void HideImages() //changes pictures to ? 
+            {
+                foreach (var pic in pictureBoxes)
+                {
+                    pic.Image = Properties.Resources.question;
+                }
+            }
+
+            private PictureBox getFreeSlot() //
+            {
+                int num;
+
+            do
+            {
+                num = rnd.Next(0, pictureBoxes.Count());
+            }     
+
+                while (pictureBoxes[num].Tag != null);
+                return pictureBoxes[num]; 
+            }
+
+            private void setRandomImages()
+            {
+                foreach (var image in images)
+                {
+                    getFreeSlot().Tag = image;
+                    getFreeSlot().Tag = image; 
+                }
+            }
+
+            private void CLICKTIMER_TICK(object sender, EventArgs e)
+            {
+                HideImages();
+                allowClick = true;
+                clickTimer.Stop(); 
+
+            }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clickImage(object sender, EventArgs e)
+        {
+            if (!allowClick) return;
+            var pic = (PictureBox)sender;
+            if (firstGuess == null)
+            {
+                firstGuess = pic;
+                pic.Image = (Image)pic.Tag;
+                return; 
+            }
+
+            pic.Image = (Image)pic.Tag;
+            if (pic.Image == firstGuess.Image && pic != firstGuess)
+            {
+                pic.Visible = firstGuess.Visible = false;
+                { firstGuess = pic;  }
+                HideImages(); 
+            }
+
+            else
+            {
+                allowClick = false;
+                clickTimer.Start();
+            }
+            //endTime = ;
+            
+            firstGuess = null;
+            if (pictureBoxes.Any(p => p.Visible)) return;
+            listBox1.Items.Add("" + label1.Text + " - " + textBox1.Text);
+            MessageBox.Show("You win! Now try again :) ");
+            ResetImages();
+        }
+
+        private void startGame(object sender, EventArgs e)
+        {
+            allowClick = true;
+            setRandomImages();
+            HideImages();
+            startGameTimer();
+            clickTimer.Interval = 1000;
+            clickTimer.Tick += CLICKTIMER_TICK;
+            btnStart.Enabled = false; 
+        }
+
+        private void instructionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 helpForm = new Form2();
+            helpForm.Show(); 
+            
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void entername(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter) 
+            {
+                textBox1.Enabled = false; 
+            }
+        }
+
+     
+    }
+}
---
+++ SuperheroMemoryGame/Form2.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace SuperheroMemoryGame
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AccessibleDescription = "";
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Agency FB", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(294, 240);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Press start to begin the game. Click on a question mark to reveal the picture beh" +
+    "ind it, match all the pictures before the time runs out to win the game! ";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 278);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
